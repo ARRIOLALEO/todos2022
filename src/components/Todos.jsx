3e2d021742aab0fdaf727420d697bf233a561f3f
mario arriola <riblarsv@gmail.com>
2022-12-16 04:09:08 +0100

    chore: add todo

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -4,12 +4,34 @@ export default class Todos extends Component{
     constructor(){
         super();
         this.state ={
-            todos:""
+            todos:[],
+            actualTodo :"" // {value:"",isDone:false, dueDate:date,subTodos:[]}
         }
     }
+    // CRUD
+
     render(){
         return(
+            <>
             <h1>Todos</h1>
+            <ul>
+                {
+                    this.state.todos.map((todo)=>{
+                        return(<li>{todo}<button>Done</button>
+                        <button onClick={()=>{
+                            // i need to delete this todo from my state
+
+                        }}>X</button>
+                        </li>)
+                    })
+                }
+            </ul>
+            <input type="text" className='input' placeholder="add your Todo" value={this.state.actualTodo} onChange={(e)=>{this.setState({actualTodo:e.target.value})}}/>
+            <button onClick={()=>{
+                this.setState({todos:[this.state.actualTodo,...this.state.todos]})
+                this.setState({actualTodo:""})
+            }}>Add Todo</button>
+            </>
         )
     }
 }
