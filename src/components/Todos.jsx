7fd53bb35c50cfd882df0dd5345d2d0a4149ef24
mario arriola <riblarsv@gmail.com>
2022-12-16 05:01:49 +0100

    chore: testing

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -5,20 +5,37 @@ export default class Todos extends Component{
         super();
         this.state ={
             todos:[],
-            actualTodo :"" // {value:"",isDone:false, dueDate:date,subTodos:[]}
+            actualTodo :{value:"",isDone:false} // {value:"",isDone:false, dueDate:date,subTodos:[]}
         }
     }
     // CRUD
 
     render(){
+        const btn={
+        width:"250px",
+        backgroundColor:'red'
+        }
+        const done ={
+            textDecoration: "line-through"
+        }
         return(
             <>
             <h1>Todos</h1>
             <ul>
                 {
                     this.state.todos.map((todo,index)=>{
-                        return(<li key={todo+index}>{todo}<button>Done</button>
-                        <button onClick={()=>{
+                        return(<li key={todo+index} >
+                            <span style={todo.isDone ? done : {}} >{todo.value}</span>
+                            <button style={btn} onClick={()=> {
+                                // we copy the state
+                                // we change the index 
+                                // than we update our state
+                                const copy = [...this.state.todos]
+                                copy[index].isDone = true
+                                console.log(copy)
+                                this.setState({todos:copy})
+                                }}>Done</button>
+                        <button style={btn} onClick={()=>{
                             const copyTodos =[...this.state.todos]
                             copyTodos.splice(index,1)
                             this.setState({todos:copyTodos})
@@ -27,11 +44,12 @@ export default class Todos extends Component{
                     })
                 }
             </ul>
-            <input type="text" className='input' placeholder="add your Todo" value={this.state.actualTodo} onChange={(e)=>{this.setState({actualTodo:e.target.value})}}/>
-            <button onClick={()=>{
+            <input type="text" className='input' placeholder="add your Todo" value={this.state.actualTodo.value} onChange={(e)=>{this.setState({actualTodo:{value:e.target.value,isDone:false}})}}/>
+
+            <button style={btn} onClick={()=>{
                 // not accept duplicate todos
                 this.setState({todos:[this.state.actualTodo,...this.state.todos]})
-                this.setState({actualTodo:""})
+                this.setState({actualTodo:{value:"",isDone:false}})
             }}>Add Todo</button>
             </>
         )
