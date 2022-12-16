a9e4a6c90211225011d69d7ba34e7c87fb4d43ed
mario arriola <riblarsv@gmail.com>
2022-12-16 03:16:42 +0100

    chore: add basic component

---
+++ src/components/Todos.jsx
@@ -0,0 +1,16 @@
+import {Component} from 'react'
+
+export default class Todos extends Component{
+    constructor(){
+        super();
+        this.state ={
+
+        }
+    }
+    render(){
+        return(
+            <h1>Todos</h1>
+        )
+    }
+}
+
